8d3a26f713528127b38b1cbbf5a687f222c8cf3c
Shivam-png-lab <shivamkv0807@gmail.com>
2026-08-18 23:45:58 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.34%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -4,11 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: bool"""
     
-        if n == 1:
-            return True
+        if n <= 0:
+            return False
 
-        while n > 0 and n % 2 == 0:
-            n = n // 2
-
-        return n == 1
+        return (n & (n - 1)) == 0
                 
